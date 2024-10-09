9ad62a19d28eda49df2fe8fc0ea6a547ba8ab1c2
Aiden-BachDuong-ICD201-01 <aiden.bachduong@student.tdsb.on.ca>
2024-10-09 19:07:54 +0000

    hgfyu

--- PP2_1.py
+++ PP2_1.py
@@ -8,24 +8,25 @@
 def q1(): 
   #Write Assignment code here
 
-  num = int(input("Input an interger: "))
-  if num%2: 
+  num = int(input("In: "))
+  if not num%2:
     print(f"{num} is even")
-  if num//2:
+  else:
     print(f"{num} is odd")
 
 
 def q2(): 
   #Write Assignment code here
-  name = input("Input a name: ")
+  name = input("In: ")
   if name == "Kalisz":
     print("teacher")
-  else:
+  if name != "Kalisz":
     print("student")
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
-
+'''
 q1()
 q2()
+'''
