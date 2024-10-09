7c73cc49df7aefbcae3510e2d7719e3aa6555f54
Aiden-BachDuong-ICD201-01 <aiden.bachduong@student.tdsb.on.ca>
2024-10-09 13:44:57 +0000

    bhbh

--- PP2_1.py
+++ PP2_1.py
@@ -1,12 +1,27 @@
-
+'''
+  Lesson: If statments
+  Author: Aiden
+  Date Created: Oct 9, 2024
+  Date Last Modified: Oct 9, 2024
+'''
 
 def q1(): 
   #Write Assignment code here
 
+  num = int(input("Input an interger: "))
+  if num%2: 
+    print(f"{num} is even")
+  if num//2:
+    print(f"{num} is odd")
+
 
 def q2(): 
   #Write Assignment code here
-
+  name = input("Input a name: ")
+  if name == "Kalisz":
+    print("teacher")
+  else:
+    print("student")
 
 
 #Do not alter the following code
